f7535952fb7abed81801233a094afbe7252a3e9e
Ivan Kuchin <i.kuchin@openproject.com>
2024-02-12 19:07:00 +0100

    wip

--- app/components/members/row_component.rb
+++ app/components/members/row_component.rb
@@ -67,7 +67,7 @@ module Members
     def roles
       span = content_tag "span", roles_label, id: "member-#{member.id}-roles"
 
-      if may_update?
+      if can_update?
         span + role_form
       else
         span
@@ -114,39 +114,59 @@ module Members
       helpers.translate_user_status(model.principal.status)
     end
 
-    def may_update?
-      table.authorize_update && member.project_role?
-    end
+    def can_update? = table.authorize_update && member.project_role?
 
-    def may_delete?
-      table.authorize_delete && member.deletable?
-    end
+    def may_delete? = table.authorize_delete
 
-    def may_delete_shares?
-      table.authorize_work_package_shares_delete && member.shared_work_packages_count?
-    end
+    def deletable_project_role? = member.project_role? && member.deletable?
 
-    def button_links
-      @button_links ||= [].tap do |links|
-        links << edit_link if may_update?
-        links << delete_link if may_delete?|| may_delete_shares?
+    def can_delete? = may_delete? && deletable_project_role?
+
+    def may_delete_shares? = table.authorize_work_package_shares_delete
+
+    def has_shared_work_packages? = member.shared_work_packages_count?
+
+    def can_delete_shares? = may_delete_shares? && has_shared_work_packages?
+
+    def actions
+      @actions ||= [].tap do |actions|
+        actions << edit_action_options if can_update?
+        actions << delete_action_options if can_delete? || can_delete_shares?
       end
     end
 
-    def edit_link
-      link_to(
-        edit_icon,
-        '#',
-        class: "toggle-membership-button #{toggle_item_class_name}",
-        'data-action': 'members-form#toggleMembershipEdit',
-        'data-members-form-toggling-class-param': toggle_item_class_name,
-        title: t(:button_edit)
-      )
+    def button_links
+      return [] if actions.empty?
+
+      if actions.one?
+        actions.first => {label:, **button_options}
+
+        [render(Primer::Beta::IconButton.new(**button_options, size: :small, "aria-label": label))]
+      else
+        [
+          render(Primer::Alpha::ActionMenu.new) do |menu|
+            menu.with_show_button(scheme: :invisible, size: :small, icon: :'kebab-horizontal', "aria-label": t(:actions))
+            actions.each do |action_options|
+              action_options => {scheme:, label:, icon:, **button_options}
+              menu.with_item(scheme:, label:, content_arguments: button_options) do |item|
+                item.with_leading_visual_icon(icon:)
+              end
+            end
+          end
+        ]
+      end
     end
 
-    def edit_icon
-      render(Primer::Beta::Octicon.new(icon: :pencil))
-      # render(Primer::Beta::IconButton.new(size: :small, icon: :pencil, "aria-label": "Edit roles"))
+    def edit_action_options
+      {
+        scheme: :default,
+        icon: :pencil,
+        label: t(:button_edit),
+        data: {
+          action: 'members-form#toggleMembershipEdit',
+          members_form_toggling_class_param: toggle_item_class_name,
+        },
+      }
     end
 
     def roles_css_id
@@ -157,19 +177,51 @@ module Members
       "member-#{member.id}--edit-toggle-item"
     end
 
-    def delete_link
-      link_to(
-        delete_icon,
-        { controller: '/members', action: 'destroy', id: model, page: params[:page] },
-        method: :delete,
-        data: { confirm: delete_link_confirmation, disable_with: I18n.t(:label_loading) },
-        title: delete_title
-      )
-    end
+    def delete_action_options
+      dialog = Primer::Alpha::Dialog.new(title: 'Remove member')
 
-    def delete_icon
-      render(Primer::Beta::Octicon.new(icon: :trash))
-      # render(Primer::Beta::IconButton.new(scheme: :danger, size: :small, icon: :trash, "aria-label": "Delete member"))
+      dialog.with_header(variant: :large)
+
+      dialog.with_body do
+        case
+        when can_delete? && can_delete_shares?
+          concat(render(Primer::Beta::Text.new(tag: 'p')) { "This will remove the user’s role from this project. However, #{member.shared_work_packages_count} work packages have also been shared with this user, possibly with different privileges." })
+          concat(render(Primer::Beta::Text.new(tag: 'p')) { 'Would you like to keep their access to shared work packages or remove all access?' })
+        when can_delete?
+          concat(render(Primer::Beta::Text.new(tag: 'p')) { 'Deleting this member will revoke all access privileges of the user to the project.' })
+        when can_delete_shares?
+          concat(render(Primer::Beta::Text.new(tag: 'p')) { "This user does not have a direct role in this project but #{member.shared_work_packages_count} work packages have been shared with them. This action will revoke their access to all shared work packages." })
+        else
+          concat(render(Primer::Beta::Text.new(tag: 'p')) { 'You cannot delete this member because they belong to a group that is itself a member of this project.' })
+          concat(render(Primer::Beta::Text.new(tag: 'p')) { 'You can either remove the group as a member of the project or this specific member from the group in the administration settings.' })
+        end
+      end
+
+      dialog.with_footer do
+        concat(render(Primer::Beta::Button.new(data: { close_dialog_id: dialog.id })) { "Cancel" })
+        case
+        when can_delete? && can_delete_shares?
+          concat(render(Primer::Beta::Button.new(scheme: :danger, tag: :a, href: delete_url(delete_member: true), data: { method: :delete })) { "Keep only shared" })
+          concat(render(Primer::Beta::Button.new(scheme: :danger, tag: :a, href: delete_url(delete_member: true, delete_shared_work_packages: true), data: { method: :delete })) { "Remove all" })
+        when can_delete?
+          concat(render(Primer::Beta::Button.new(scheme: :danger, tag: :a, href: delete_url(delete_member: true), data: { method: :delete })) { "Remove" })
+        when can_delete_shares?
+          concat(render(Primer::Beta::Button.new(scheme: :danger, tag: :a, href: delete_url(delete_shared_work_packages: true), data: { method: :delete })) { "Remove" })
+        end
+      end
+
+      content_for :content_body do
+        render(dialog)
+      end
+
+      {
+        scheme: :danger,
+        icon: :trash,
+        label: I18n.t(:button_remove),
+        data: {
+          show_dialog_id: dialog.id,
+        },
+      }
     end
 
     def delete_title
@@ -180,10 +232,8 @@ module Members
       end
     end
 
-    def delete_link_confirmation
-      if !User.current.admin? && model.include?(User.current)
-        t(:text_own_membership_delete_confirmation)
-      end
+    def delete_url(delete_member: nil, delete_shared_work_packages: nil)
+      url_for(controller: '/members', action: 'destroy_by_principal', principal_id: principal, delete_member:, delete_shared_work_packages:)
     end
 
     def column_css_class(column)
